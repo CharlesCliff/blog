cb75e7eada8f37d254967d7b2dbd543fc17d2ab6
陈杨 <cliffchen@sohu-inc.com>
2018-03-30 00:17:48 +0800

    chage name

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,7 @@ export TZ='Asia/Shanghai'
 
 # 先 clone 再 commit，避免直接 force commit
 # 不然整个 branch 就总是只有一个 commit，不好看
-git clone -b master git@github.com:CharlesCliff/CharlesCliff.github.io.git .deploy_git
+git clone -b master git@github.com:charlescliff/charlescliff.github.io.git .deploy_git
 
 cd .deploy_git
 git checkout master
